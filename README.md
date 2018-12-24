f29c3a0fb93e459bd6f4c7252dc9f5f8503d971a
Elliott Morris <elliott@Elliotts-MacBook-Pro.local>
2018-12-23 21:39:34 -0600

    box scores

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # rNBA
+
+This is a repo to house code for parsing NBA data, inclduing computing time-series ELO scores, box scores, team contribution, etc. I may one day use this data to make a Bayesian forecasting model for NBA games.... but for now, it's more of a learning tool than anything. 
+
+Data in the `data/` directory:
+
+* Scores for every game played in the `/data/schedules` directory, which is organized by season.
+* Player box scores and play-by-play data for every game ever, in the `data/game data` directory, organized by game. **This data is stored in the `.Rdata` format, due to having lists of data frames for each game in each season.
+
+ 
--- scripts/scrape_gamedata.R
+++ scripts/scrape_gamedata.R
@@ -5,6 +5,16 @@
 
 
 # Scrape schedule and scores ----------------------------------------------
+# identify years already downloaded
+already_downloaded <- lapply(dir('data/schedules/'),function(x){substr(x,5,8)}) %>% unlist()
+
+# download all years
+years <- as.character(seq(2018,2010,-1)) # earliest is 1950
+
+years <- years[!years %in% already_downloaded]
+
+# redo this season every time
+#years <- append('2019',years)
 
 # input year as string
 download_yearly_game_data <- function(year){
@@ -16,7 +26,6 @@ download_yearly_game_data <- function(year){
   ########
   monthList <- tolower(month.name)
   playoff_startDate <- ymd("2018-04-14")
-  outputfile <- sprintf("data/NBA-%s_game_data.csv",year)
   
   ########
   # SCRIPT FOR SCRAPING DATA STARTS HERE
@@ -80,25 +89,15 @@ download_yearly_game_data <- function(year){
   df$box_score_text <- NULL
   
   # save to file
-  write.csv(df, outputfile,row.names = F)
+  write.csv(df,
+            sprintf("data/schedules/NBA-%s_schedule.csv",year),
+            row.names = F)
   
   # return file
   return(df)
 }
 
 #download_yearly_game_data(year='2019')
-
-# identify years already downloaded
-already_downloaded <- lapply(dir('data'),function(x){substr(x,5,8)}) %>% unlist()
-
-# download all years
-years <- as.character(seq(2018,2010,-1)) # earliest is 1950
-
-years <- years[!years %in% already_downloaded]
-
-# redo this season every time
-years <- append('2019',years)
-
 
 # dont do parallel, will exhaust scraping limits
 for (x in years){
@@ -123,11 +122,6 @@ for (x in years){
 #
 # To use this script for other seasons, run 2018-12-11_nba_game_data.R for a 
 # different year, then use that output file as the input for this script.
-
-
-
-inputfile <- "../data/NBA-2018_game_data.rds"
-outputfile <- "../data/NBA-2018_box_score.rds"
 
 # remove the +'s from play-by-play score
 parseScore <- function(x) {
@@ -158,86 +152,120 @@ parseBoxScore <- function(xx) {
   xx
 }
 
+# identify years already downloaded
+already_downloaded <- lapply(dir('data/game data/'),function(x){substr(x,5,8)}) %>% unlist()
+
+# download all years
+years <- as.character(seq(2018,2010,-1)) # earliest is 1950
+
+years <- years[!years %in% already_downloaded]
+
+# for this season, make sure to update new games -- have to read the schedule file and game file and compare
+#years <- append('2019',years)
+
 # time to acrually scrape
-scrape_box_scores <- function(inputfile){
-  game_df <- as.tibble(readRDS(inputfile))
+scrape_box_scores <- function(year){
+  print("##############################")
+  print(sprintf("Scraping %s...",year))
   
+  season <- sprintf("NBA-%s_schedule.csv",year)
+
+  game_df <- read.csv(sprintf('data/schedules/%s',season),stringsAsFactors = F)
+  
+  # trim to only finished games, duh
+  game_df <- game_df %>% filter(!is.na(home_pts))
+  
+  # get every game score, in parallel
   master <- list()
   
-  for (current_id in game_df$game_id) {
-    print(current_id)
-    
-    ##########
-    # get box scores
-    ##########
-    url <- paste0("https://www.basketball-reference.com/boxscores/", current_id,
-                  ".html")
-    webpage <- read_html(url)
-    
-    tables <- webpage %>% html_nodes("table") %>%
-      html_table()
-    names(tables) <- c("visitor_basic_boxscore", "visitor_adv_boxscore",
-                       "home_basic_boxscore", "home_adv_boxscore")
-    tables <- lapply(tables, parseBoxScore)
-    
-    ##########
-    # get play-by-play score profile
-    ##########
-    url <- paste0("https://www.basketball-reference.com/boxscores/pbp/", current_id,
-                  ".html")
-    webpage <- read_html(url)
-    
-    # pull out the events from the play-by-play table
-    events <- webpage %>% 
-      html_nodes("#pbp") %>%
-      html_nodes("tr") %>% 
-      html_text()
-    
-    # get event times & scores
-    times  <- str_extract(events, "^\\d+:\\d+.\\d+")
-    scores <- str_extract(events, "[\\+]*\\d+-\\d+[\\+]*")
-    scores <- ifelse(str_detect(scores, "\\+"), scores, NA)
-    
-    pdp_df <- data.frame(time = times, score = scores, stringsAsFactors = FALSE) %>%
-      na.omit()
-    pdp_df$score <- sapply(pdp_df$score, parseScore)
-    
-    # split score into visitor and home score, get home advantage
-    pdp_df <- pdp_df %>% 
-      separate(score, into = c("visitor", "home"), sep = "-") %>%
-      mutate(visitor = as.numeric(visitor), 
-             home = as.numeric(home),
-             time = ms(time))
-    
-    # get period of play (e.g. Q1, Q2, ...)
-    pdp_df$period <- NA
-    period <- 0
-    prev_time <- ms("0:00")
-    for (i in 1:nrow(pdp_df)) {
-      curr_time <- pdp_df[i, "time"]
-      if (prev_time < curr_time) {
-        period <- period + 1
-      }
-      pdp_df[i, "period"] <- period
-      prev_time <- curr_time
-    }
-    
-    # convert time such that it runs upwards. regular quarters are 12M long, OT 
-    # periods are 5M long
-    pdp_df <- pdp_df %>% mutate(time = ifelse(period <= 4, 
-                                              as.duration(12 * 60) - as.duration(time),
-                                              as.duration(5  * 60) - as.duration(time))) %>%
-      mutate(time = ifelse(period <= 4,
-                           time + as.duration(12 * 60 * (period - 1)),
-                           time + as.duration(12 * 60 * 4) + 
-                             as.duration(5 * 60 * (period - 5))
-      ))
-    
-    tables$pdp_df <- pdp_df
-    
-    master[[current_id]] <- tables
-  }
+  cl = makeCluster(detectCores()-1,type="FORK")
   
-  #saveRDS(master, outputfile)
+  pblapply(game_df$game_id,cl=cl,
+           function(current_id){
+             print(current_id)
+    
+            ##########
+            # get box scores
+            ##########
+            url <- paste0("https://www.basketball-reference.com/boxscores/", current_id,
+                          ".html")
+            webpage <- read_html(url)
+            
+            tables <- webpage %>% html_nodes("table") %>%
+              html_table()
+            names(tables) <- c("visitor_basic_boxscore", "visitor_adv_boxscore",
+                               "home_basic_boxscore", "home_adv_boxscore")
+            tables <- lapply(tables, parseBoxScore)
+            
+            ##########
+            # get play-by-play score profile
+            ##########
+            url <- paste0("https://www.basketball-reference.com/boxscores/pbp/", current_id,
+                          ".html")
+            webpage <- read_html(url)
+            
+            # pull out the events from the play-by-play table
+            events <- webpage %>% 
+              html_nodes("#pbp") %>%
+              html_nodes("tr") %>% 
+              html_text()
+            
+            # get event times & scores
+            times  <- str_extract(events, "^\\d+:\\d+.\\d+")
+            scores <- str_extract(events, "[\\+]*\\d+-\\d+[\\+]*")
+            scores <- ifelse(str_detect(scores, "\\+"), scores, NA)
+            
+            pdp_df <- data.frame(time = times, score = scores, stringsAsFactors = FALSE) %>%
+              na.omit()
+            pdp_df$score <- sapply(pdp_df$score, parseScore)
+            
+            # split score into visitor and home score, get home advantage
+            pdp_df <- pdp_df %>% 
+              separate(score, into = c("visitor", "home"), sep = "-") %>%
+              mutate(visitor = as.numeric(visitor), 
+                     home = as.numeric(home),
+                     time = ms(time))
+            
+            # get period of play (e.g. Q1, Q2, ...)
+            pdp_df$period <- NA
+            period <- 0
+            prev_time <- ms("0:00")
+            for (i in 1:nrow(pdp_df)) {
+              curr_time <- pdp_df[i, "time"]
+              if (prev_time < curr_time) {
+                period <- period + 1
+              }
+              pdp_df[i, "period"] <- period
+              prev_time <- curr_time
+            }
+            
+            # convert time such that it runs upwards. regular quarters are 12M long, OT 
+            # periods are 5M long
+            pdp_df <- pdp_df %>% mutate(time = ifelse(period <= 4, 
+                                                      as.duration(12 * 60) - as.duration(time),
+                                                      as.duration(5  * 60) - as.duration(time))) %>%
+              mutate(time = ifelse(period <= 4,
+                                   time + as.duration(12 * 60 * (period - 1)),
+                                   time + as.duration(12 * 60 * 4) + 
+                                     as.duration(5 * 60 * (period - 5))
+              ))
+            
+            tables$pdp_df <- pdp_df
+            
+            master[[current_id]] <- tables
+          }
+  )
   
+  stopCluster(cl)
+  
+  saveRDS(master, sprintf("data/game data/NBA-%s_game_data.rds",year))
+  
+  
+}
+
+for (x in years){
+  scrape_box_scores(year=x)
+  
+  print("Sleeping...")
+  Sys.sleep(10)
 }
